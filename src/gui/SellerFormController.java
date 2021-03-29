6be0eedc3e5f7634768643a48a6a61e6f7d4f34d
tiagopires79 <tiagopiresdb@gmail.com>
2021-03-29 17:34:00 -0300

    TextField & DatePicker

--- src/gui/SellerFormController.java
+++ src/gui/SellerFormController.java
@@ -1,8 +1,11 @@
 package gui;
 
 import java.net.URL;
+import java.time.LocalDate;
+import java.time.ZoneId;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Locale;
 import java.util.Map;
 import java.util.ResourceBundle;
 import java.util.Set;
@@ -17,6 +20,7 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
+import javafx.scene.control.DatePicker;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import model.entities.Seller;
@@ -25,35 +29,41 @@ import model.services.SellerService;
 
 public class SellerFormController implements Initializable{
 
-	private Seller entity;
-	
-	private SellerService dService;
-	
+	private Seller entity;	
+	private SellerService dService;	
 	private List<DataChangeListener> dataChangeListeners = new ArrayList<>();
 		
 	@FXML
-	private TextField txtId;
-	
+	private TextField txtId;	
 	@FXML
-	private TextField txtName;
-	
+	private TextField txtName;	
+	@FXML
+	private TextField txtEmail;	
+	@FXML
+	private DatePicker dpBirthDate;	
+	@FXML
+	private TextField txtBaseSalary;	
 	@FXML
 	private Label labelErrorName;
-	
 	@FXML
-	private Button btSave;
-	
+	private Label labelErrorEmail;
+	@FXML
+	private Label labelErrorBirthDate;
+	@FXML
+	private Label labelErrorBaseSalary;
+	@FXML
+	private Button btSave;	
 	@FXML
 	private Button btCancel;
 	
 	public void setSeller(Seller entity) {
 		this.entity = entity;
-	}
 	
+	}	
 	public void setSellerService(SellerService dService) {
 		this.dService = dService;
-	}
 	
+	}	
 	public void subscribeDataChangeListener(DataChangeListener listener) {
 		dataChangeListeners.add(listener);
 	}
@@ -64,6 +74,12 @@ public class SellerFormController implements Initializable{
 		}
 		txtId.setText(String.valueOf(entity.getId()));
 		txtName.setText(entity.getName());
+		txtEmail.setText(entity.getEmail());
+		Locale.setDefault(Locale.US);
+		txtBaseSalary.setText(String.format("%.2f", entity.getBaseSalary()));
+		if (entity.getBirthDate() != null) {
+			dpBirthDate.setValue(LocalDate.ofInstant(entity.getBirthDate().toInstant(), ZoneId.systemDefault()));
+		}
 	}
 	
 	@FXML
@@ -121,7 +137,10 @@ public class SellerFormController implements Initializable{
 	
 	private void initializeNodes() {
 		Constraints.setTextFieldInteger(txtId);
-		Constraints.setTextFieldMaxLength(txtName, 50);
+		Constraints.setTextFieldMaxLength(txtName, 70);
+		Constraints.setTextFieldDouble(txtBaseSalary);
+		Constraints.setTextFieldMaxLength(txtEmail, 60);
+		Utils.formatDatePicker(dpBirthDate, "dd/MM/yyyy");
 	}
 	
 	private void setErrorMessages(Map<String, String> errors) {
